3f235a969a600c09a2fdd807a0e8f6eed94729f9
Shraya <shraya978@gmail.com>
2026-01-05 11:55:23 +0530

    initial commit

---
+++ DemoGit/src/com/sample/UserValidation.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+public class UserValidation {
+	public static boolean isValidName(String name) {
+		
+	}
+	public static void amin(String[] args) {
+		String name = "ASjhfdhbf";
+	}
+}
