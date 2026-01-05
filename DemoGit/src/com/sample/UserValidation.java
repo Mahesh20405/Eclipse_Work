e762916e3f9bcf73d3826ec355622413461d24e4
Shraya05 <163320293+Shraya05@users.noreply.github.com>
2026-01-05 12:28:41 +0530

    Update UserValidation.java

--- DemoGit/src/com/sample/UserValidation.java
+++ DemoGit/src/com/sample/UserValidation.java
@@ -2,7 +2,8 @@ package com.sample;
 
 public class UserValidation {
 	public static boolean isValidName(String name) {
-		
+		if (name.length() > 8) return true;
+		return false;
 	}
 	public static void amin(String[] args) {
 		String name = "ASjhfdhbf";
